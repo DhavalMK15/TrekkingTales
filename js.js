4b9f83cd76fc45723f7f5b7b81103a9e2188b70a
Dhaval Kotak <139615422+DhavalMK15@users.noreply.github.com>
2023-07-18 01:53:06 +0530

    Add files via upload

---
+++ js.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+    $('.Ladakh').hide();
+    $('#btn1').click(function () {
+        $('.Ladakh').fadeToggle(700);
+    });
+
+    $('.Manali').hide();
+    $('#btn2').click(function () {
+        $('.Manali').fadeToggle(700);
+    });
+
+    $('.Chalal').hide();
+    $('#btn3').click(function () {
+        $('.Chalal').fadeToggle(700);
+    });
+
+    $('.Chadar').hide();
+    $('#btn4').click(function () {
+        $('.Chadar').fadeToggle(700);
+    });
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
